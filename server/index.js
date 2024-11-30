5cf37befc4c57429ace47bce01ae3d8e2bf9aa44
Tomas Jibaja <tomasjibaja@gmail.com>
2024-11-30 01:51:52 -0300

    modificando el CORS de indes.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.listen(port, () => console.log(`Node server started at port ${port}`));
 // Aplicar los middleware
 app.use(cors())
 app.use(cors({
-  origin: ['https://kinta-health-backend.vercel.app'],
+  origin: ['https://kinta-health-backend.app'],
   methods: ['POST', 'GET'],
   credentials: true
 }))
